fc7efaad02afc43b671053bc0bca44127ebeac15
Ryan Clark <Rymaster95@gmail.com>
2017-02-08 14:17:34 -0700

    completed mini project

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var massive = require('massive');
+var connectionString = require("./config.js")
+var massiveInstance = massive.connectSync({connectionString : connectionString.connectionString})
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
@@ -6,7 +9,18 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
-})
+app.set('db', massiveInstance);
+var db = app.get('db');
+var controller = require('./controller.js');
+// db.new_plane(function(err, planes){
+//     console.log(err, "plane added")
+// });
+controller.getPlanes();
+// db.get_planes(function(err, planes){
+//     console.log(err, planes)
+// })
 
+
+app.listen('3000', function(){
+  console.log("Successfully listening on : 3000")
+})
